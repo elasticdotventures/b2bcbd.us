066767cc4e13201e86fbbaab2e50f03775a91afe
Brian Horakh <brianh@elastic.ventures>
2019-06-10 12:00:28 +1000

    migrated website to vue-cli

--- sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws s3 sync --size-only --acl public-read --delete --exclude '.*' /projects/b2bcbd/b2bcbd.us 's3://b2bcbd.us/'
-aws cloudfront create-invalidation --distribution-id E25I7ANYGTLI54 --paths /
-
